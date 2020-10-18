5a608df1233c1c17e479c84e0b22c759367690fa
Parply <alex_pinches@btinternet.com>
2020-10-18 23:23:24 +0100

    cv2 rgba with interp

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import base64
 import requests
 import traceback
 from wordcloud import WordCloud,ImageColorGenerator
-from matplotlib import pyplot as plt
+import cv2
 
 load_dotenv()
 ghtoken = os.getenv('INPUT_GH_TOKEN')
@@ -74,10 +74,10 @@ class get_files:
                         self.files.append(j)
 
     def contents(self):
-        self.text = ""
+        self.textb64 = b''
         for i in self.files:
-            print(i)
-            self.text += str(base64.b64decode(i.content),'utf-8')
+            self.textb64 += base64.b64decode(i.content)
+        self.text = str(self.textb64,'utf-8')
         return self.text
 def decode_readme(data: str):
     '''Decode the contents of old readme'''
@@ -85,7 +85,7 @@ def decode_readme(data: str):
     return str(decoded_bytes, 'utf-8')
 def generate_new_readme(readme: str,username : str):
     '''Generate a new Readme.md'''
-    r1 = f"<p align='center'><img src='https://raw.githubusercontent.com/{username}/{username}/master/wordcloud/wordcloud.png' alt='Maths and Programming Addict' width='{WIDTH}'/></p>"
+    r1 = f"<p align='center'><img src='https://raw.githubusercontent.com/{username}/{username}/master/wordcloud/wordcloud.png' alt='Word cloud generated from my GitHub repositories' width='{WIDTH}'/></p>"
     r = f"{START_COMMENT}\n {r1} \n {END_COMMENT}"
     return re.sub(listReg,r,readme)  
 if __name__=="__main__":            
@@ -94,11 +94,11 @@ if __name__=="__main__":
         gg =get_files(g)
         gg.get_files()
         text = gg.contents()
-        wordcloud = WordCloud(colormap=icmap,collocations=False,background_color=None,mode="RGBA",width=1000,height=400).generate(text)
-        #plt.figure(figsize=(10,4))
-        #plt.imshow(wordcloud,interpolation="bilinear")
-        #plt.axis("off")
-        #plt.savefig("wordcloud.png")
+        wordcloud = WordCloud(colormap=icmap,collocations=False,background_color=None,mode="RGBA",width=1200,height=480).generate(text)
+        arr=wordcloud.to_array()
+        img = cv2.resize(arr,(1000,400),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite('wordcloud.png',img)
+
         headers = {"Authorization": "Bearer " + ghtoken}
         user_data = run_query(userInfoQuery) 
         
@@ -109,7 +109,7 @@ if __name__=="__main__":
        
         repo = g.get_repo(f"{username}/{username}")
         star_me(username)
-        l="""
+        
         committer = InputGitAuthor(author f"{author}@example.com")
         with open('wordcloud.png', 'rb') as input_file:
             data = input_file.read()
@@ -118,18 +118,18 @@ if __name__=="__main__":
             repo.update_file(contents.path, "WordCloud Updated", data, contents.sha, committer=committer,branch=branch)
         except Exception as e:
             repo.create_file("wordcloud/wordcloud.png", "WordCloud Added", data, committer=committer)
-        """
+        
         
         contents = repo.get_readme(ref=branch)
         rdmd =decode_readme(contents.content)
         new_readme = generate_new_readme(rdmd, username)
-        r="""
+        
         if new_readme != rdmd:
             repo.update_file(path=contents.path, message="Added Wordcloud",
                              content=new_readme, sha=contents.sha, branch=branch,
                              committer=committer)
             print("Readme updated")
-    """
+   
     except Exception as e:
         traceback.print_exc()
         print("Exception Occurred " + str(e))
